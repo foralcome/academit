7b1a95b9b5e5b972ca1b3735dcb289ff83903b6a
foralcome <foralcome@mail.ru>
2018-03-30 09:02:44 +0700

    update main

--- CourseTasks/CSV/CsvMain.cs
+++ CourseTasks/CSV/CsvMain.cs
@@ -16,13 +16,16 @@ namespace Academits.Barsukov
                 Console.WriteLine("В программу должно быть передано 2 аргумента через пробел:");
                 Console.WriteLine("1) путь к CSV-файлу");
                 Console.WriteLine("2) путь к HTML-файлу для сохранения");
-            }
 
-            CsvParser p = new CsvParser(args[0], 3, ',', Encoding.UTF8);
-            if (p.ParseInHtmlFile(args[1]))
+            }
+            else
             {
-                Console.WriteLine("Операция чтения файла CSV выполнена успешно! Обработано {0} строк(и).", p.GetCountRecords());
-                System.Diagnostics.Process.Start(args[1]);
+                CsvParser p = new CsvParser(args[0], 3, ',', Encoding.UTF8);
+                if (p.ParseInHtmlFile(args[1]))
+                {
+                    Console.WriteLine("Операция чтения файла CSV выполнена успешно! Обработано {0} строк(и).", p.GetCountRecords());
+                    System.Diagnostics.Process.Start(args[1]);
+                }
             }
         }
     }
