db85e505d84baf24e2eb48a31696599443b79610
foralcome <foralcome@mail.ru>
2018-04-03 16:43:43 +0700

    load arraylist

---
+++ CourseTasks/ArrayList/ArrayLisMain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academits.Barsukov
+{
+    class ArrayLisMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Создание списка целочисленных значений");
+            ArrayList<int> parts = new ArrayList<int>(10);
+            parts.Add(11);
+            parts.Add(12);
+            parts.Add(13);
+            parts.Add(14);
+            parts.Add(15);
+            parts.Add(16);
+            parts.Add(17);
+            parts.Add(18);
+            parts.Add(19);
+            parts.Add(20);
+            Console.WriteLine("Список: {0}", parts.ToString());
+            Console.WriteLine("Элементов: {0}", parts.Count);
+            Console.WriteLine("Вместимость: {0}", parts.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("1) Изменение Capacity на 20 ");
+            parts.Capacity = 20;
+            Console.WriteLine("Список: {0}", parts.ToString());
+            Console.WriteLine("Элементов: {0}", parts.Count);
+            Console.WriteLine("Вместимость: {0}", parts.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("2) Получение значения списка по индексу 3 ");
+            Console.WriteLine("Значение: {0}", parts[3]);
+            Console.WriteLine();
+
+            Console.WriteLine("3) Изменение Copacity по фактическому количеству элементов в списке");
+            parts.TrimExcess();
+            Console.WriteLine("Список: {0}", parts.ToString());
+            Console.WriteLine("Элементов: {0}", parts.Count);
+            Console.WriteLine("Вместимость: {0}", parts.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("4) Добавление нового элемента 21 в конец списка");
+            parts.Add(21);
+            Console.WriteLine("Список: {0}", parts.ToString());
+            Console.WriteLine("Элементов: {0}", parts.Count);
+            Console.WriteLine("Вместимость: {0}", parts.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("5) Добавление нового элемента 22 по индексу 2");
+            parts.Insert(2,22);
+            Console.WriteLine("Список: {0}", parts.ToString());
+            Console.WriteLine("Элементов: {0}", parts.Count);
+            Console.WriteLine("Вместимость: {0}", parts.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("6) Удаление элемента по индексу 0");
+            parts.RemoveAt(0);
+            Console.WriteLine("Список: {0}", parts.ToString());
+            Console.WriteLine("Элементов: {0}", parts.Count);
+            Console.WriteLine("Вместимость: {0}", parts.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("7) Удаление элемента по значению 16");
+            parts.Remove(16);
+            Console.WriteLine("Список: {0}", parts.ToString());
+            Console.WriteLine("Элементов: {0}", parts.Count);
+            Console.WriteLine("Вместимость: {0}", parts.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("8) Удаление диапазона элементов с 1 по 5 индекс");
+            parts.RemoveRange(1, 5);
+            Console.WriteLine("Список: {0}", parts.ToString());
+            Console.WriteLine("Элементов: {0}", parts.Count);
+            Console.WriteLine("Вместимость: {0}", parts.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("9) Проверка наличия элемента в списке по значению");
+            Console.WriteLine("Результат проверки элемента 12: {0}", parts.Contains(12));
+            Console.WriteLine("Результат проверки элемента 14: {0}", parts.Contains(14));
+            Console.WriteLine();
+
+            Console.WriteLine("10) Поиск элемента в списке и получение его индекса");
+            Console.WriteLine("Результат проверки элемента 12: {0}", parts.IndexOf(12));
+            Console.WriteLine("Результат проверки элемента 14: {0}", parts.IndexOf(14));
+            Console.WriteLine();
+
+            Console.WriteLine("11) Копирование списка в массив");
+            Console.WriteLine("Исходный список: {0}", parts.ToString());
+            int[] arrayParts = new int[parts.Count];
+            parts.CopyTo(arrayParts, 1);
+            Console.WriteLine("Скопированный массив: ");
+            bool isFirst = true;
+            Console.Write("{");
+            foreach (int a in arrayParts)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                }
+                else
+                {
+                    Console.Write(", ");
+                }
+                Console.Write(a);
+            }
+            Console.Write("}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CourseTasks/ArrayList/ArrayList.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academits.Barsukov
+{
+    class ArrayList<T> : IList<T>
+    {
+        private T[] items;
+        private int length;
+        private int capacity;
+
+        public ArrayList(int capacity)
+        {
+            if (capacity < 1)
+            {
+                throw new ArgumentOutOfRangeException("размер нового списка должен больше количества элементов находящихся в нём!");
+            }
+
+            this.items = new T[capacity];
+            this.Capacity = capacity;
+        }
+
+        public int Count
+        {
+            get { return length; }
+        }
+
+        //Размер этого массива
+        public int Capacity
+        {
+            get { return capacity; }
+            set
+            {
+                if (value < Count)
+                {
+                    throw new ArgumentOutOfRangeException("размер нового списка должен не меньше количества элементов в нём!");
+                }
+                else
+                {
+                    capacity = value;
+                    SetCapacity(capacity);
+                }
+            }
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        private void IncreaseCapacity()
+        {
+            T[] old = items;
+            this.capacity = old.Length * 2;
+            items = new T[this.capacity];
+            Array.Copy(old, 0, items, 0, old.Length);
+        }
+
+        private void SetCapacity(int capacity)
+        {
+            if (capacity < this.Count)
+            {
+                throw new ArgumentOutOfRangeException("новый размер списка должен быть больше количества элементов находящихся в нём!");
+            }
+            T[] old = items;
+            this.capacity = capacity;
+            items = new T[capacity];
+            Array.Copy(old, 0, items, 0, this.Count);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException("индекс вышел за пределы списка");
+                }
+                else
+                {
+                    return items[index];
+                }
+            }
+            set
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException("индекс вышел за пределы списка");
+                }
+                else
+                {
+                    items[index] = value;
+                }
+            }
+        }
+
+        public void TrimExcess()
+        {
+            T[] old = items;
+            items = new T[this.Count];
+            Array.Copy(old, 0, items, 0, this.Count);
+            this.Capacity = this.Count;
+        }
+
+        public void Add(T obj)
+        {
+            if (this.Count + 1 >= this.Capacity)
+            {
+                IncreaseCapacity();
+            }
+            items[this.Count] = obj;
+            ++length;
+        }
+
+        public void Insert(int index, T obj)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException("индекс вышел за пределы списка");
+            }
+
+            if (this.Count + 1 >= this.Capacity)
+            {
+                IncreaseCapacity();
+            }
+
+            Array.Copy(items, index, items, index + 1, this.Count - index);
+            items[index] = obj;
+            ++length;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException("индекс вышел за пределы списка");
+            }
+
+            if (this.Count + 1 >= this.Capacity)
+            {
+                IncreaseCapacity();
+            }
+
+            Array.Copy(items, index + 1, items, index, this.Count - index - 1);
+            --length;
+            Array.Clear(items, length, items.Length - length);
+        }
+
+        public bool Remove(T value)
+        {
+            int indexSearch = -1;
+            for (int index = 0; index < this.Count; index++)
+            {
+                if (items[index].Equals(value))
+                {
+                    indexSearch = index;
+                    break;
+                }
+            }
+            if (indexSearch == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(indexSearch);
+            return true;
+        }
+
+        public void RemoveRange(int indexStart, int indexStop)
+        {
+            if (indexStart >= indexStop)
+            {
+                throw new IndexOutOfRangeException("начальный индекс должен быть меньше конечного!");
+            }
+
+            if (indexStart < 0 || indexStop >= this.Count)
+            {
+                throw new IndexOutOfRangeException("индекс вышел за пределы списка");
+            }
+
+            T[] old = items;
+            this.length -= (indexStop - indexStart + 1);
+            items = new T[this.Capacity];
+            Array.Copy(old, 0, items, 0, indexStart);
+            Array.Copy(old, indexStop + 1, items, indexStart, old.Length - indexStop - 1);
+            Array.Clear(items, length, old.Length - length);
+        }
+
+        public void Clear()
+        {
+            Array.Clear(items, 0, items.Length);
+            length = 0;
+        }
+
+        public bool Contains(T value)
+        {
+            for (int index = 0; index < this.Count; index++)
+            {
+                if (items[index].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(T value)
+        {
+            for (int index = 0; index < this.Count; index++)
+            {
+                if (items[index].Equals(value))
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        public void CopyTo(T[] array, int index)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("свойство array имеет значение null");
+            }
+
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException("индекс вышел за пределы списка");
+            }
+
+            Array.Copy(items, index, array, 0, this.Count - index);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder r = new StringBuilder();
+            bool isFirst = true;
+            r.Append("{");
+            foreach (T a in items)
+            {
+                if (!isFirst)
+                {
+                    r.Append(", ");
+                }
+                r.Append(a);
+                isFirst = false;
+            }
+            r.Append("}");
+            return r.ToString();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var el in items)
+                yield return el;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        public override bool Equals(Object o)
+        {
+            if (ReferenceEquals(o, this))
+            {
+                return true;
+            }
+            if (ReferenceEquals(o, null) || o.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            ArrayList<T> p = (ArrayList<T>) o;
+            if (this.Capacity != p.Capacity || this.Count!=p.Count)
+            {
+                return false;
+            }
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (!this.items[i].Equals(p.items[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 151;
+            int hash = 1;
+            hash = prime * hash + Count;
+            foreach (T v in this.items)
+            {
+                hash = prime * hash + v.GetHashCode();
+            }
+
+            return hash;
+        }
+    }
+}
